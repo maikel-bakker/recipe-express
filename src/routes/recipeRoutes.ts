279b64f7203fdd109617cb06295b8c99e9b2efaf
maikel-bakker <maikel.bakker1991@gmail.com>
2018-11-28 11:47:37 +0100

    [TASK] Remove general route

--- src/routes/recipeRoutes.ts
+++ src/routes/recipeRoutes.ts
@@ -1,5 +1,4 @@
 import { RecipeController } from '../controllers/recipeController';
-import { Request, Response } from 'express';
 
 export class RecipeRoutes {
     recipeController : RecipeController = new RecipeController();  
@@ -7,12 +6,5 @@ export class RecipeRoutes {
     public routes(app): void {          
         app.route('/get-recipes').get(this.recipeController.getRecipes); 
         app.route('/add-recipe').post(this.recipeController.addRecipe);
-         
-        app.route('/')
-        .get((req: Request, res: Response) => {            
-            res.status(200).send({
-                message: 'GET request successfulll!!!!'
-            })
-        });  
     }
 }
