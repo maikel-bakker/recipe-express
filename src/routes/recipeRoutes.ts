12ec9439775dde1bea989f8ef28bcd18a6a7a96c
maikel-bakker <maikel.bakker1991@gmail.com>
2018-12-02 12:33:05 +0100

    [FEATURE] Added route to get single recipe by id

--- src/routes/recipeRoutes.ts
+++ src/routes/recipeRoutes.ts
@@ -4,7 +4,9 @@ export class RecipeRoutes {
     recipeController : RecipeController = new RecipeController();  
         
     public routes(app): void {          
-        app.route('/get-recipes').get(this.recipeController.getRecipes); 
+        app.route('/get-recipes').get(this.recipeController.getRecipes);
+        app.route('/get-recipe/:recipeId').get(this.recipeController.getSingleRecipe);
+        
         app.route('/add-recipe').post(this.recipeController.addRecipe);
     }
 }
